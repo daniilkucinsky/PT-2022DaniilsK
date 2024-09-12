c6e0a9dcae6da47aba1e9858e64b03ea00bef503
Daniils <your@email.ex>
2024-09-12 10:50:57 +0300

    outer join testing

--- select2join/outerjoin.sql
+++ select2join/outerjoin.sql
@@ -1,28 +1,37 @@
 -- outer join
 
-use sql_store;
+-- use sql_store;
 
 -- inner join 
-select * from customers c
-join orders o
-using(customer_id);
+-- select * from customers c
+-- join orders o
+-- using(customer_id);
 
 -- left outer join from customer
-select * from customers c
-left join orders o
-using(customer_id);
+-- select * from customers c
+-- left join orders o
+-- using(customer_id);
 
 -- left outer join from order
-select * from orders o
-left join customers c
-using(customer_id);
+-- select * from orders o
+-- left join customers c
+-- using(customer_id);
 
 -- right outer join 
-select * from customers c
-right join orders o
-using(customer_id);
+-- select * from customers c
+-- right join orders o
+-- using(customer_id);
 
 -- right outer join 
-select * from orders o
-right join customers c
-using(customer_id);
+-- select * from orders o
+-- right join customers c
+-- using(customer_id);
+use sql_store;
+
+select p.product_id, oi.quantity, p.name from products p
+join order_items oi
+on oi.product_id = p.product_id;
+
+select * from products p
+left join order_items oi
+using(product_id);
